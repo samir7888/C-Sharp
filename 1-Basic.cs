9c225abd5adc214edb79800d024c97cf97412be7
Sameer Basnet <basnetsameer78@gmail.com>
2025-01-18 23:25:47 +0545

    first commit

---
+++ 1-Basic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        //to enter name from user
+    Console.WriteLine("Enter your name: ");
+    string userName  =  Console.ReadLine();
+      Console.WriteLine("Your name is "+userName);    
+        
+    }
+  }
+}
