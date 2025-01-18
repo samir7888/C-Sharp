515566ae2fc14613f7ddbbb0d52811d1e3e9ff87
Sameer Basnet <basnetsameer78@gmail.com>
2025-01-18 23:59:22 +0545

    rename

--- 2-strings.cs
+++ 2-strings.cs
@@ -6,10 +6,10 @@ namespace StringExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Check the size of your D");
+            Console.WriteLine("Check the size of your Snake");
             Console.WriteLine("Enter letters as much as you can:");
             string size = Console.ReadLine();
-            Console.WriteLine("The length of your input is: " + size.Length);
+            Console.WriteLine("The length of your snake is: " + size.Length);
         }
     }
 }
