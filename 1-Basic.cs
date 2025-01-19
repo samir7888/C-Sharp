e650950c7c089ba9c551211922dc839446b34f19
Sameer Basnet <basnetsameer78@gmail.com>
2025-01-19 16:04:20 +0545

    Done with array

--- 1-Basic.cs
+++ 1-Basic.cs
@@ -1,16 +1,16 @@
-﻿using System;
+﻿// using System;
 
-namespace HelloWorld
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      //to enter name from user
-      Console.WriteLine("Enter your name: ");
-      string userName = Console.ReadLine();
-      Console.WriteLine("Your name is " + userName);
+// namespace HelloWorld
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       //to enter name from user
+//       Console.WriteLine("Enter your name: ");
+//       string userName = Console.ReadLine();
+//       Console.WriteLine("Your name is " + userName);
 
-    }
-  }
-}
+//     }
+//   }
+// }
--- 2-strings.cs
+++ 2-strings.cs
@@ -6,10 +6,34 @@ namespace StringExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Check the size of your Snake");
-            Console.WriteLine("Enter letters as much as you can:");
-            string size = Console.ReadLine();
-            Console.WriteLine("The length of your snake is: " + size.Length);
+            // Console.WriteLine("Check the size of your Snake");
+            // Console.WriteLine("Enter letters as much as you can:");
+            // string size = Console.ReadLine();
+            // Console.WriteLine("The length of your snake is: " + size.Length);
+
+            //foreach loop
+
+            // string[] cars = { "tesla", "bmw", "rosroyels" };
+            // foreach (string i in cars)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+
+
+
+            //2D-Array
+
+            int[,] numbers = { { 1, 2, 3 }, { 4, 5, 6 } };
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+
+                    Console.WriteLine(numbers[i, j]);
+                }
+
+            }
         }
     }
 }
